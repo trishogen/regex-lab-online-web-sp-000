04461e1c94a884822e3a49a7a389e7c9b5647ff8
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 02:29:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\s[a-z]{5}\s/x)
+  text.scan(/[a-z]{5}/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
