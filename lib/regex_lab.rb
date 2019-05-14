1858afd323af75ea8279d2a405944ca6d656e79a
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 03:03:10 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if /\b[aeiou][a-z]*/i.match(word)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if /\A[A-Z].*[.?!]\z/.match(text)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if /\b[()]?\d{3}[()\s-]?\d{3}[()\s-]?\d{4}\b/.match(phone)
+    true
+  else
+    false
+  end
 end
