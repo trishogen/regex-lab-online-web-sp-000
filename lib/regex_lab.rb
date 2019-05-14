efc8284b884eecd0b448c6caf4997108a0b3f816
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 02:14:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/[\A\s][a-z]{5}[\s/]\z)
+  text.scan(/[\A\s][a-z]{5}[\s\z]/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
