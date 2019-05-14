defd55d09fd2711156ec2df4372b6fcd62c8346c
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 02:45:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  /\A[A-Z].*[.?!]\z/.match(text)
+  if /\A[A-Z].*[.?!]\z/.match(text)
+    true
+  end
 end
 
 def valid_phone_number?(phone)
