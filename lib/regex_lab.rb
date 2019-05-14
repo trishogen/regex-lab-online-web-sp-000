176340d317cca6f6bb9caa3b43681e25a9f4eadb
Trish Ogen <patriciaogen@gmail.com>
2019-05-14 02:05:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun[a-z]*ing/)
+  /\Aun[a-z]*ing/i.scan(text)
 end
 
 def words_five_letters_long(text)
